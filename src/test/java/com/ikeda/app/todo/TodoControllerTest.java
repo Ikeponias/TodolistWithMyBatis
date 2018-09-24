a66448217934db4f9fd725d023e3eeb73bd6709c
ikeponias <sephiroth929@gmail.com>
2018-09-24 13:47:26 +0900

    feat(ControllerTest)

---
+++ src/test/java/com/ikeda/app/todo/TodoControllerTest.java
@@ -0,0 +1,52 @@
+package com.ikeda.app.todo;
+
+import java.util.List;
+
+import com.ikeda.domain.model.Todo;
+import com.ikeda.domain.service.TodoService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TodoControllerTest {
+  private MockMvc mockMvc;
+
+  @Autowired
+  TodoController todoController;
+
+  @MockBean
+  TodoService todoService;
+
+  @Before
+  public void setup() {
+    mockMvc = MockMvcBuilders.standaloneSetup(todoController).build();
+  }
+
+  @Test
+  public void Indexのリクエストに対してレスポンスのステータスコードが正しいか() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/todo")).andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  public void Indexのリクエストに対してIndexのViewを返しているか() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/todo")).andExpect(MockMvcResultMatchers.view().name("todo/index"));
+  }
+
+  @Test
+  public void Indexのリクエストに対してModelに正しいtodosを詰められているか() throws Exception {
+    List<Todo> todos = todoService.selectAll();
+    mockMvc.perform(MockMvcRequestBuilders.get("/todo"))
+        .andExpect(MockMvcResultMatchers.model().attribute("todos", todos));
+  }
+}
