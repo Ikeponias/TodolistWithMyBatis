75ffaa983a048aa1eba88330c2dd85a66ae3584d
ikeponias <sephiroth929@gmail.com>
2018-10-01 21:31:00 +0900

    fix(WeiComment)

--- src/main/java/com/ikeda/app/todo/TodoAPIController.java
+++ src/main/java/com/ikeda/app/todo/TodoAPIController.java
@@ -7,15 +7,14 @@ import com.ikeda.domain.service.TodoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 
-@Controller
+@RestController
 @RequestMapping(value = "/api/todo")
 public class TodoAPIController {
 
@@ -23,14 +22,12 @@ public class TodoAPIController {
   TodoService todoService;
 
   @GetMapping(value = "", produces = "application/json")
-  @ResponseBody
   @ApiOperation(value = "全てのTODOを取得する", notes = "JSON形式で返る")
   public ResponseEntity<List<Todo>> indexJson() {
     return ResponseEntity.ok(todoService.selectAll());
   }
 
   @GetMapping(value = "/{id}", produces = "application/json")
-  @ResponseBody
   @ApiOperation(value = "IDを与えてTODOを取得する", notes = "JSON形式で返る")
   public ResponseEntity<Todo> showJson(@PathVariable("id") Long id) {
     return ResponseEntity.ok(todoService.selectById(id));
--- src/main/java/com/ikeda/mapper/TodoMapper.java
+++ src/main/java/com/ikeda/mapper/TodoMapper.java
@@ -3,18 +3,19 @@ package com.ikeda.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.ikeda.domain.model.Todo;
 
 @Mapper
 public interface TodoMapper {
-	boolean insert(Todo todo);
+	boolean insert(@Param("todo") Todo todo);
 
-	boolean update(Long id, Todo todo);
+	boolean update(@Param("id") Long id, @Param("todo") Todo todo);
 
-	Todo selectById(Long id);
+	Todo selectById(@Param("id") Long id);
 
 	List<Todo> selectAll();
 
-	boolean deleteById(Long id);
+	boolean deleteById(@Param("id") Long id);
 }
